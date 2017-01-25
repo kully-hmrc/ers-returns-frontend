ea4872be1301b379c69e86b55552159e99b85779
Matthew Planchant <matthew.planchant@digital.hmrc.gov.uk>
2017-01-25 15:22:59 +0000

    DDCNLS-444: Fixes even more tests broken by Play 2.5 upgrade.

--- test/controllers/auth/ErsRegimeSpec.scala
+++ test/controllers/auth/ErsRegimeSpec.scala
@@ -17,11 +17,13 @@
 package controllers.auth
 
 import java.util.UUID
+
 import org.mockito.Matchers
 import org.mockito.Mockito._
 import org.scalatest.BeforeAndAfterEach
 import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.{OneServerPerSuite, PlaySpec}
+import play.api.mvc.{Action, AnyContent}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import uk.gov.hmrc.domain._
@@ -30,16 +32,21 @@ import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
 import uk.gov.hmrc.play.frontend.auth.connectors.domain._
 import uk.gov.hmrc.play.frontend.controller.FrontendController
 import uk.gov.hmrc.play.http.SessionKeys
+
 import scala.concurrent.Future
 
 class ErsRegimeSpec extends PlaySpec with OneServerPerSuite with MockitoSugar with BeforeAndAfterEach {
 
+  private val orgAccount = Some(OrgAccount("org/1234", Org("1234")))
+  private val saAccount = Some(SaAccount("", SaUtr("1234567890")))
+  private val vatAccount = Some(VatAccount("", Vrn("123456789")))
+  private val epayeAccount = Some(EpayeAccount("", EmpRef("000", "AA00000")))
+  private val authority = Authority("", Accounts(), None, None, CredentialStrength.Strong, ConfidenceLevel.L50, None, None, None, "")
+  private val randomToken = "RANDOMTOKEN"
+
   "ERSRegime" must {
-
     "define isAuthorised" must {
-
       val accounts = mock[Accounts](RETURNS_DEEP_STUBS)
-
       "return true when epaye is defined" in {
         when(accounts.epaye.isDefined).thenReturn(true)
         ERSRegime.isAuthorised(accounts) must be(true)
@@ -49,9 +56,7 @@ class ErsRegimeSpec extends PlaySpec with OneServerPerSuite with MockitoSugar wi
         when(accounts.epaye.isDefined).thenReturn(false)
         ERSRegime.isAuthorised(accounts) must be(false)
       }
-
     }
-
     "define the authentication type as the epaye GG" in {
       ERSRegime.authenticationType must be(ErsFileValidatorGovernmentGateway)
     }
@@ -61,14 +66,12 @@ class ErsRegimeSpec extends PlaySpec with OneServerPerSuite with MockitoSugar wi
     }
   }
 
+
   "when applying the ERSRegime" must {
-
     val mockAuthConnector = mock[AuthConnector]
-
     object TestController extends FrontendController with Actions {
-      val authConnector = mockAuthConnector
-
-      def testRoute = AuthorisedFor(ERSRegime, pageVisibility = GGConfidence) {
+      val authConnector: AuthConnector = mockAuthConnector
+      def testRoute: Action[AnyContent] = AuthorisedFor(ERSRegime, pageVisibility = GGConfidence) {
         implicit user =>
           implicit request =>
             Ok
@@ -76,40 +79,22 @@ class ErsRegimeSpec extends PlaySpec with OneServerPerSuite with MockitoSugar wi
     }
 
     "logged-in users" must {
-
+      val userId = s"user-${UUID.randomUUID}"
+      val sessionId = s"session-${UUID.randomUUID}"
+      val request = FakeRequest().withSession(SessionKeys.sessionId -> sessionId, SessionKeys.token -> randomToken, SessionKeys.userId -> userId)
       "with a psa account" must {
-
         "allow the user access to the page" in {
-          val userId = s"user-${UUID.randomUUID}"
-          val sessionId = s"session-${UUID.randomUUID}"
-          lazy val request = FakeRequest().withSession(
-            SessionKeys.sessionId -> sessionId,
-            SessionKeys.token -> "RANDOMTOKEN",
-            SessionKeys.userId -> userId)
-
-          when(mockAuthConnector.currentAuthority(Matchers.any())) thenReturn {
-            Future.successful(Some(Authority(userId, Accounts(sa = Some(SaAccount("",SaUtr("1234567890"))), vat = Some(VatAccount("", Vrn("123456789"))), epaye = Some(EpayeAccount("", EmpRef("000", "AA00000")))), None, None,CredentialStrength.Strong, ConfidenceLevel.L50, None, None)))
-          }
-
+          val newAuthority = authority.copy(uri = userId, accounts = Accounts(sa = saAccount, vat = vatAccount, epaye = epayeAccount))
+          when(mockAuthConnector.currentAuthority(Matchers.any())) thenReturn Future.successful(Some(newAuthority))
           val result = TestController.testRoute.apply(request)
           status(result) must be(OK)
         }
       }
 
       "without a psa account" must {
-
         "redirect the user to the unauthorised page" in {
-
-          val userId = s"user-${UUID.randomUUID}"
-          val sessionId = s"session-${UUID.randomUUID}"
-          lazy val request = FakeRequest().withSession(
-            SessionKeys.sessionId -> sessionId,
-            SessionKeys.token -> "RANDOMTOKEN",
-            SessionKeys.userId -> userId)
-
-          when(mockAuthConnector.currentAuthority(Matchers.any())) thenReturn {
-            Future.successful(Some(Authority(userId, Accounts(org = Some(OrgAccount("org/1234", Org("1234")))), None, None, CredentialStrength.Strong, ConfidenceLevel.L50, None, None)))
-          }
+          val newAuthority = authority.copy(uri = userId, accounts = Accounts(org = orgAccount))
+          when(mockAuthConnector.currentAuthority(Matchers.any())) thenReturn Future.successful(Some(newAuthority))
           val result = TestController.testRoute.apply(request)
           status(result) must be(SEE_OTHER)
           redirectLocation(result) must be(Some("/submit-your-ers-annual-return/unauthorised"))
@@ -118,7 +103,6 @@ class ErsRegimeSpec extends PlaySpec with OneServerPerSuite with MockitoSugar wi
     }
 
     "not logged-in users" must {
-
       "redirect to the gg sign-in page" in {
         val result = TestController.testRoute.apply(FakeRequest())
         status(result) must be(SEE_OTHER)
